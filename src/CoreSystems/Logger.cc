4f18705c8194444256fd33c632cddf9e866f13c2
DawidSzpejna <dawid.szpejna13@gmail.com>
2023-10-09 23:38:49 +0200

    logger: add logger and context classes

---
+++ src/CoreSystems/Logger.cc
@@ -0,0 +1,45 @@
+#include <stdexcept>
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#include "Logger.hh"
+
+namespace Esper
+{
+    Logger *Logger::_s_instance = nullptr;
+
+    Logger::Logger()
+    {
+        if (Logger::_s_instance != nullptr)
+        {
+            throw std::runtime_error("The logger instance already exists!");
+        }
+
+        Logger::_s_instance = this;
+    }
+
+    Logger::~Logger()
+    {
+        Logger::_s_instance = nullptr;
+    }
+
+    std::unique_ptr<Logger> Logger::create()
+    {
+        /* create singleton */
+        auto logger = std::unique_ptr<Logger>{new Logger()};
+
+        /* set pattern of messages */
+        spdlog::set_pattern("%^[%T][%n]%$ : %v");
+
+        /* set multithread logger for trace level */
+        logger->_m_core_logger = spdlog::stdout_color_mt("Esper");
+        logger->_m_core_logger->set_level(spdlog::level::trace);
+
+        /* set multithread logger for client level */
+        logger->_m_client_logger = spdlog::stdout_color_mt("Client");
+        logger->_m_client_logger->set_level(spdlog::level::trace);
+
+        EP_CORE_INFO("Logger is created");
+        return logger;
+    }
+}
---
+++ src/CoreSystems/Logger.hh
@@ -0,0 +1,62 @@
+#ifndef __ENGINE__LOGGER_HH__
+#define __ENGINE__LOGGER_HH__
+
+#include <memory>
+#include <spdlog/spdlog.h>
+
+namespace Esper
+{
+    class Logger
+    {
+    private:
+        static Logger *_s_instance;
+
+    private:
+        std::shared_ptr<spdlog::logger> _m_core_logger;
+        std::shared_ptr<spdlog::logger> _m_client_logger;
+
+        Logger();
+
+    public:
+        ~Logger();
+        static std::unique_ptr<Logger> create();
+
+        inline static Logger *get_instance() { return Logger::_s_instance; }
+        inline static std::shared_ptr<spdlog::logger> &get_core_logger() { return Logger::_s_instance->_m_core_logger; }
+        inline static std::shared_ptr<spdlog::logger> &get_client_logger() { return Logger::_s_instance->_m_client_logger; }
+    };
+
+} // namespace Esper
+
+/* ################## LOGGER MACROS ################################# */
+#ifdef NDEBUG
+/* FOR CORE */
+#define EP_CORE_TRACE(...)
+#define EP_CORE_INFO(...)
+#define EP_CORE_WARM(...)
+#define EP_CORE_ERROR(...)
+#define EP_CORE_CRITICAL(...)
+
+/* FOR CLIENT */
+#define EP_TRACE(...)
+#define EP_INFO(...)
+#define EP_WARM(...)
+#define EP_ERROR(...)
+#define EP_CRITICAL(...)
+#else
+/* FOR CORE */
+#define EP_CORE_TRACE(...) Esper::Logger::get_core_logger()->trace(__VA_ARGS__)
+#define EP_CORE_INFO(...) Esper::Logger::get_core_logger()->info(__VA_ARGS__)
+#define EP_CORE_WARM(...) Esper::Logger::get_core_logger()->warm(__VA_ARGS__)
+#define EP_CORE_ERROR(...) Esper::Logger::get_core_logger()->error(__VA_ARGS__)
+#define EP_CORE_CRITICAL(...) Esper::Logger::get_core_logger()->critical(__VA_ARGS__)
+
+/* FOR CLIENT */
+#define EP_TRACE(...) Esper::Logger::get_client_logger()->trace(__VA_ARGS__)
+#define EP_INFO(...) Esper::Logger::get_client_logger()->info(__VA_ARGS__)
+#define EP_WARM(...) Esper::Logger::get_client_logger()->warm(__VA_ARGS__)
+#define EP_ERROR(...) Esper::Logger::get_client_logger()->error(__VA_ARGS__)
+#define EP_CRITICAL(...) Esper::Logger::get_client_logger()->critical(__VA_ARGS__)
+#endif
+
+#endif /* __ENGINE__LOGGER_HH__ */
