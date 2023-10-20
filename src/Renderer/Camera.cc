fbbb51abcb2555408823829bc10024c6dcf4ea05
sebastianSzarafin <sebastian.szarafin@interia.pl>
2023-10-21 01:37:26 +0200

    camera: use glm functions to construct projection mat

--- src/Renderer/Camera.cc
+++ src/Renderer/Camera.cc
@@ -9,13 +9,7 @@ namespace esp
                                           float near,
                                           float far)
   {
-    m_projection_mat       = glm::mat4{ 1.0f };
-    m_projection_mat[0][0] = 2.f / (right - left);
-    m_projection_mat[1][1] = 2.f / (bottom - top);
-    m_projection_mat[2][2] = 1.f / (far - near);
-    m_projection_mat[3][0] = -(right + left) / (right - left);
-    m_projection_mat[3][1] = -(bottom + top) / (bottom - top);
-    m_projection_mat[3][2] = -near / (far - near);
+    m_projection_mat = glm::orthoLH(left, right, bottom, top, near, far);
   }
 
   void renderer::Camera::set_perspective(float fov,
@@ -25,13 +19,10 @@ namespace esp
   {
     assert(glm::abs(aspect_ratio - std::numeric_limits<float>::epsilon()) >
            0.0f);
-    const float tan_half_fov_y = tan(fov / 2.f);
-    m_projection_mat           = glm::mat4{ 0.0f };
-    m_projection_mat[0][0]     = 1.f / (aspect_ratio * tan_half_fov_y);
-    m_projection_mat[1][1]     = 1.f / (tan_half_fov_y);
-    m_projection_mat[2][2]     = far / (far - near);
-    m_projection_mat[2][3]     = 1.f;
-    m_projection_mat[3][2]     = -(far * near) / (far - near);
+
+    m_projection_mat = glm::perspective(fov, aspect_ratio, near, far);
+    m_projection_mat[2][2] *= -1;
+    m_projection_mat[2][3] *= -1;
   }
 
   void renderer::Camera::set_perspective(float aspect_ratio)
