63ce41d7ec8bc3f0bf45433f59934fa0b525a48f
sebastianSzarafin <sebastian.szarafin@interia.pl>
2023-10-20 21:54:10 +0200

    clang-format: reformat code

--- src/Renderer/Camera.cc
+++ src/Renderer/Camera.cc
@@ -2,10 +2,14 @@
 
 namespace esp
 {
-  void renderer::Camera::set_orthographic(float left, float right, float top,
-                                          float bottom, float near, float far)
+  void renderer::Camera::set_orthographic(float left,
+                                          float right,
+                                          float top,
+                                          float bottom,
+                                          float near,
+                                          float far)
   {
-    m_projection_mat = glm::mat4{ 1.0f };
+    m_projection_mat       = glm::mat4{ 1.0f };
     m_projection_mat[0][0] = 2.f / (right - left);
     m_projection_mat[1][1] = 2.f / (bottom - top);
     m_projection_mat[2][2] = 1.f / (far - near);
@@ -14,21 +18,24 @@ namespace esp
     m_projection_mat[3][2] = -near / (far - near);
   }
 
-  void renderer::Camera::set_perspective(float fov, float aspect_ratio,
-                                         float near, float far)
+  void renderer::Camera::set_perspective(float fov,
+                                         float aspect_ratio,
+                                         float near,
+                                         float far)
   {
-    assert(glm::abs(aspect_ratio - std::numeric_limits<float>::epsilon()) > 0.0f);
+    assert(glm::abs(aspect_ratio - std::numeric_limits<float>::epsilon()) >
+           0.0f);
     const float tan_half_fov_y = tan(fov / 2.f);
-    m_projection_mat = glm::mat4{ 0.0f };
-    m_projection_mat[0][0] = 1.f / (aspect_ratio * tan_half_fov_y);
-    m_projection_mat[1][1] = 1.f / (tan_half_fov_y);
-    m_projection_mat[2][2] = far / (far - near);
-    m_projection_mat[2][3] = 1.f;
-    m_projection_mat[3][2] = -(far * near) / (far - near);
+    m_projection_mat           = glm::mat4{ 0.0f };
+    m_projection_mat[0][0]     = 1.f / (aspect_ratio * tan_half_fov_y);
+    m_projection_mat[1][1]     = 1.f / (tan_half_fov_y);
+    m_projection_mat[2][2]     = far / (far - near);
+    m_projection_mat[2][3]     = 1.f;
+    m_projection_mat[3][2]     = -(far * near) / (far - near);
   }
 
   void renderer::Camera::set_perspective(float aspect_ratio)
   {
     set_perspective(m_fov, aspect_ratio, m_near, m_far);
   }
-}
+} // namespace esp
--- src/Renderer/Camera.hh
+++ src/Renderer/Camera.hh
@@ -8,20 +8,25 @@ namespace esp::renderer
   class Camera
   {
    private:
-    glm::mat4 m_projection_mat{1.f};
+    glm::mat4 m_projection_mat{ 1.f };
 
-    float m_fov{glm::radians(50.f)};
-    float m_near{.1f};
-    float m_far{100.f};
+    float m_fov{ glm::radians(50.f) };
+    float m_near{ .1f };
+    float m_far{ 100.f };
 
    public:
-    Camera() = default;
+    Camera()  = default;
     ~Camera() = default;
 
-    Camera(const Camera&) = delete;
+    Camera(const Camera&)            = delete;
     Camera& operator=(const Camera&) = delete;
 
-    void set_orthographic(float left, float right, float top, float bottom, float near, float far);
+    void set_orthographic(float left,
+                          float right,
+                          float top,
+                          float bottom,
+                          float near,
+                          float far);
     void set_perspective(float fov, float aspect_ratio, float near, float far);
     void set_perspective(float aspect_ratio);
 
@@ -34,6 +39,6 @@ namespace esp::renderer
     inline float get_far() { return m_far; }
     inline const glm::mat4& get_projection() { return m_projection_mat; }
   };
-}
+} // namespace esp::renderer
 
 #endif // RENDERER_CAMERA_H
