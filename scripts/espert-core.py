ee6fe08fb3d9c1dfcd91d30200de0a5e7eb3c7c2
kubo11 <jakub.obroslak@onet.pl>
2023-10-22 19:19:29 +0200

    scripts: export compile commands by default

--- scripts/espert-core.py
+++ scripts/espert-core.py
@@ -54,7 +54,7 @@ def get_wsi_type() -> str:
 
 
 def get_configure_command(args: Namespace) -> str:
-    CMD = "cmake -S . -B build"
+    CMD = "cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 
     if args.build_type == BuildType.DEBUG:
         CMD += f" -DCMAKE_BUILD_TYPE=Debug"
@@ -77,9 +77,6 @@ def get_configure_command(args: Namespace) -> str:
 
     if args.build_tests:
         CMD += f" -DESP_BUILD_TESTS=ON"
-
-    if args.export_compile_commands:
-        CMD += " -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 
     return CMD
 
@@ -183,14 +180,6 @@ def get_parser() -> ArgumentParser:
         default=False,
         action="store_true",
         help="Clean out build directory first.",
-    )
-    main_parser.add_argument(
-        "-e",
-        "--export-compile-commands",
-        required=False,
-        default=False,
-        action="store_true",
-        help="Add CMAKE_EXPORT_COMPILE_COMMANDS to build.",
     )
     main_parser.add_argument(
         "-t",
