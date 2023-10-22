b8af62a4e9a55312114d4e72c9a40ca848f21e2e
kubo11 <jakub.obroslak@onet.pl>
2023-10-22 18:43:26 +0200

    scritps: reformat scripts with black formatter

--- scripts/espert-core.py
+++ scripts/espert-core.py
@@ -19,24 +19,24 @@ import subprocess
 import sys
 
 GET_WSI_COMMAND = "loginctl show-session $(loginctl | grep $(whoami) | awk '{print $1}') -p Type | cut -d'=' -f2"
-CTEST_FILE = 'CTestTestfile.cmake'
+CTEST_FILE = "CTestTestfile.cmake"
 
 
 class WSI(Enum):
-    XCM = 'xcm'
-    XLIB = 'xlib'
-    WAYLAND = 'wayland'
-    D2D = 'd2d'
+    XCM = "xcm"
+    XLIB = "xlib"
+    WAYLAND = "wayland"
+    D2D = "d2d"
 
 
 class BuildType(Enum):
-    DEBUG = 'debug'
-    RELEASE = 'release'
+    DEBUG = "debug"
+    RELEASE = "release"
 
 
 class Compiler(Enum):
-    GCC = 'gcc'
-    CLANG = 'clang'
+    GCC = "gcc"
+    CLANG = "clang"
 
 
 def get_cpu_count() -> int:
@@ -50,41 +50,42 @@ def get_number_of_jobs() -> int:
 def get_wsi_type() -> str:
     proc = subprocess.Popen(GET_WSI_COMMAND, shell=True, stdout=subprocess.PIPE)
     proc.wait()
-    return proc.stdout.read().decode('utf-8').strip()
+    return proc.stdout.read().decode("utf-8").strip()
 
 
 def get_configure_command(args: Namespace) -> str:
-    CMD = 'cmake -S . -B build'
+    CMD = "cmake -S . -B build"
 
     if args.build_type == BuildType.DEBUG:
-        CMD += f' -DCMAKE_BUILD_TYPE=Debug'
+        CMD += f" -DCMAKE_BUILD_TYPE=Debug"
     elif args.build_type == BuildType.RELEASE:
-        CMD += f' -DCMAKE_BUILD_TYPE=Release'
+        CMD += f" -DCMAKE_BUILD_TYPE=Release"
 
     if args.compiler == Compiler.GCC:
-        CMD += ' -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++'
+        CMD += " -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++"
     elif args.compiler == Compiler.CLANG:
-        CMD += ' -DCMAKE_C_COMPILER=clang-17 -DCMAKE_CXX_COMPILER=clang++-17'
+        CMD += " -DCMAKE_C_COMPILER=clang-17 -DCMAKE_CXX_COMPILER=clang++-17"
 
-    if sys.platform.startswith('linux'):
+    if sys.platform.startswith("linux"):
         if args.wsi is None:
             wsi = get_wsi_type()
         else:
             wsi = args.wsi.value
-        if wsi == 'x11': wsi = 'xlib'
-        CMD += f' -DVKB_WSI_SELECTION={wsi.upper()}'
+        if wsi == "x11":
+            wsi = "xlib"
+        CMD += f" -DVKB_WSI_SELECTION={wsi.upper()}"
 
     if args.build_tests:
-        CMD += f' -DESP_BUILD_TESTS=ON'
+        CMD += f" -DESP_BUILD_TESTS=ON"
 
     if args.export_compile_commands:
-        CMD += ' -DCMAKE_EXPORT_COMPILE_COMMANDS=ON'
+        CMD += " -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 
     return CMD
 
 
 def get_build_command(args) -> str:
-    CMD = f'make -j{args.jobs}'
+    CMD = f"make -j{args.jobs}"
 
     return CMD
 
@@ -121,91 +122,109 @@ def run_tests(args: Namespace) -> None:
         args.build_tests = True
         run_build(args)
 
-    test_command = 'ctest'
+    test_command = "ctest"
     run_command_detached(test_command, BUILD_DIR)
 
 
 def get_parser() -> ArgumentParser:
-    main_parser = ArgumentParser(prog='espert-core',
-                                 description='Manage espert-core project.')
+    main_parser = ArgumentParser(
+        prog="espert-core", description="Manage espert-core project."
+    )
 
-    subparsers = main_parser.add_subparsers(dest='cmd')
-    configure_parser = subparsers.add_parser('configure',
-                                             help='Configure project with cmake.')
+    subparsers = main_parser.add_subparsers(dest="cmd")
+    configure_parser = subparsers.add_parser(
+        "configure", help="Configure project with cmake."
+    )
     configure_parser.set_defaults(func=run_configure)
-    build_parser = subparsers.add_parser('build',
-                                         help='Build project.')
+    build_parser = subparsers.add_parser("build", help="Build project.")
     build_parser.set_defaults(func=run_build)
-    test_parser = subparsers.add_parser('test',
-                                        help='Run tests.')
+    test_parser = subparsers.add_parser("test", help="Run tests.")
     test_parser.set_defaults(func=run_tests)
 
     build_type_group = main_parser.add_mutually_exclusive_group()
-    build_type_group.add_argument('-d',
-                                  '--debug',
-                                  action='store_const',
-                                  dest='build_type',
-                                  const=BuildType.DEBUG,
-                                  help='Use debug configuration. (default)')
-    build_type_group.add_argument('-r',
-                                  '--release',
-                                  action='store_const',
-                                  dest='build_type',
-                                  const=BuildType.RELEASE,
-                                  help='Use release configuration.')
+    build_type_group.add_argument(
+        "-d",
+        "--debug",
+        action="store_const",
+        dest="build_type",
+        const=BuildType.DEBUG,
+        help="Use debug configuration. (default)",
+    )
+    build_type_group.add_argument(
+        "-r",
+        "--release",
+        action="store_const",
+        dest="build_type",
+        const=BuildType.RELEASE,
+        help="Use release configuration.",
+    )
     main_parser.set_defaults(build_type=BuildType.DEBUG)
-    
-    compiler_selection_group = main_parser.add_mutually_exclusive_group()
-    compiler_selection_group.add_argument('--gcc',
-                                          action='store_const',
-                                          dest='compiler',
-                                          const=Compiler.GCC,
-                                          help='Pick gcc and g++ as project compilers. (default)')
-    compiler_selection_group.add_argument('--clang',
-                                          action='store_const',
-                                          dest='compiler',
-                                          const=Compiler.CLANG,
-                                          help='Pick clang and clang++ as project compilers.')
 
-    main_parser.add_argument('-c',
-                             '--clean',
-                             required=False,
-                             default=False,
-                             action='store_true',
-                             help='Clean out build directory first.')
-    main_parser.add_argument('-e',
-                             '--export-compile-commands',
-                             required=False,
-                             default=False,
-                             action='store_true',
-                             help='Add CMAKE_EXPORT_COMPILE_COMMANDS to build.')
-    main_parser.add_argument('-t',
-                             '--build-tests',
-                             required=False,
-                             default=False,
-                             action='store_true',
-                             help='Build tests for project.')
-    main_parser.add_argument('-j',
-                              '--jobs',
-                              required=False,
-                              type=int,
-                              default=get_number_of_jobs(),
-                              help='Number of paralell jobs during build. (default: max system threads - 2)')
-    main_parser.add_argument('-w',
-                             '--wsi',
-                             required=False,
-                             type=WSI,
-                             action=EnumAction,
-                             help='Select WSI type for linux systems.')
-    
+    compiler_selection_group = main_parser.add_mutually_exclusive_group()
+    compiler_selection_group.add_argument(
+        "--gcc",
+        action="store_const",
+        dest="compiler",
+        const=Compiler.GCC,
+        help="Pick gcc and g++ as project compilers. (default)",
+    )
+    compiler_selection_group.add_argument(
+        "--clang",
+        action="store_const",
+        dest="compiler",
+        const=Compiler.CLANG,
+        help="Pick clang and clang++ as project compilers.",
+    )
+
+    main_parser.add_argument(
+        "-c",
+        "--clean",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Clean out build directory first.",
+    )
+    main_parser.add_argument(
+        "-e",
+        "--export-compile-commands",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Add CMAKE_EXPORT_COMPILE_COMMANDS to build.",
+    )
+    main_parser.add_argument(
+        "-t",
+        "--build-tests",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Build tests for project.",
+    )
+    main_parser.add_argument(
+        "-j",
+        "--jobs",
+        required=False,
+        type=int,
+        default=get_number_of_jobs(),
+        help="Number of paralell jobs during build. (default: max system threads - 2)",
+    )
+    main_parser.add_argument(
+        "-w",
+        "--wsi",
+        required=False,
+        type=WSI,
+        action=EnumAction,
+        help="Select WSI type for linux systems.",
+    )
+
     return main_parser
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
     if args.cmd is None:
-        print('Command not given.')
+        print("Command not given.")
         parser.print_usage()
         sys.exit(1)
     args.func(args)
