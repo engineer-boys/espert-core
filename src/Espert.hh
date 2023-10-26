67f381066c63b846cf43074724bc4acaaaacdcaf
sebastianSzarafin <sebastian.szarafin@interia.pl>
2023-10-26 20:02:59 +0200

    renderAPI: delete old vulkan specific render api and model component

--- src/Espert.hh
+++ src/Espert.hh
@@ -16,10 +16,6 @@
 #include "Core/ClientPoint.hh"
 #include "Renderer/Camera.hh"
 #include "Scene/Components/Components.hh"
-#include "VulkanRenderAPI/EspDescriptors.hh"
-#include "VulkanRenderAPI/EspPipeline.hh"
-#include "VulkanRenderAPI/EspPushConstantData.hh"
-#include "VulkanRenderAPI/EspRenderContext.hh"
 
 /* ------------------------------------------ */
 
