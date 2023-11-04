437890784cdfafb0f3d753f51f8b8413ed63e910
DawidSzpejna <dawid.szpejna13@gmail.com>
2023-11-04 17:55:40 +0100

    abstract-rednerAPI: upgrade RenderAPI for uniforms

---
+++ src/Core/RenderAPI/Uniforms/EspUniformManager.hh
@@ -0,0 +1,24 @@
+#ifndef CORE_RENDER_API_ESP_UNIFORM_MANAGER_HH
+#define CORE_RENDER_API_ESP_UNIFORM_MANAGER_HH
+
+#include "esppch.hh"
+
+namespace esp
+{
+  struct EspUniformManager
+  {
+   public:
+    virtual ~EspUniformManager() {}
+
+    virtual void attach() const = 0;
+
+    virtual EspUniformManager&
+    update_buffer_uniform(uint32_t set, uint32_t binding, uint64_t offset, uint32_t size, void* data) = 0;
+
+    virtual EspUniformManager&
+    load_texture(uint32_t set, uint32_t binding, uint64_t offset, std::string path_to_texture) = 0;
+  };
+
+} // namespace esp
+
+#endif /* CORE_RENDER_API_ESP_UNIFORM_MANAGER_HH */
