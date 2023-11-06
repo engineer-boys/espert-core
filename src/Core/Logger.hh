fc54908efed0c3387e0e397d5e383b2bbe34c94b
kubo11 <jakub.obroslak@onet.pl>
2023-11-06 03:19:35 +0100

    logger: fix macro typo

--- src/Core/Logger.hh
+++ src/Core/Logger.hh
@@ -33,28 +33,28 @@ namespace esp
 /* FOR CORE */
 #define ESP_CORE_TRACE(...)
 #define ESP_CORE_INFO(...)
-#define ESP_CORE_WARM(...)
+#define ESP_CORE_WARN(...)
 #define ESP_CORE_ERROR(...)
 #define ESP_CORE_CRITICAL(...)
 
 /* FOR CLIENT */
 #define ESP_TRACE(...)
 #define ESP_INFO(...)
-#define ESP_WARM(...)
+#define ESP_WARN(...)
 #define ESP_ERROR(...)
 #define ESP_CRITICAL(...)
 #else
 /* FOR CORE */
 #define ESP_CORE_TRACE(...)    esp::Logger::get_core_logger()->trace(__VA_ARGS__)
 #define ESP_CORE_INFO(...)     esp::Logger::get_core_logger()->info(__VA_ARGS__)
-#define ESP_CORE_WARM(...)     esp::Logger::get_core_logger()->warm(__VA_ARGS__)
+#define ESP_CORE_WARN(...)     esp::Logger::get_core_logger()->warn(__VA_ARGS__)
 #define ESP_CORE_ERROR(...)    esp::Logger::get_core_logger()->error(__VA_ARGS__)
 #define ESP_CORE_CRITICAL(...) esp::Logger::get_core_logger()->critical(__VA_ARGS__)
 
 /* FOR CLIENT */
 #define ESP_TRACE(...)         esp::Logger::get_client_logger()->trace(__VA_ARGS__)
 #define ESP_INFO(...)          esp::Logger::get_client_logger()->info(__VA_ARGS__)
-#define ESP_WARM(...)          esp::Logger::get_client_logger()->warm(__VA_ARGS__)
+#define ESP_WARN(...)          esp::Logger::get_client_logger()->warn(__VA_ARGS__)
 #define ESP_ERROR(...)         esp::Logger::get_client_logger()->error(__VA_ARGS__)
 #define ESP_CRITICAL(...)      esp::Logger::get_client_logger()->critical(__VA_ARGS__)
 #endif
