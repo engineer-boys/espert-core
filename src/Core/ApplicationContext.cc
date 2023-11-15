89d44f046db78d3fae8122c5141589818b18a15b
kubo11 <jakub.obroslak@onet.pl>
2023-11-16 00:24:07 +0100

    resource system: change base asset path

--- src/Core/ApplicationContext.cc
+++ src/Core/ApplicationContext.cc
@@ -20,7 +20,7 @@ namespace esp
     /* create context dependencies */
     {
       context->m_logger          = Logger::create();
-      context->m_resource_system = ResourceSystem::create(fs::current_path() / "resources");
+      context->m_resource_system = ResourceSystem::create(fs::current_path().parent_path() / "resources");
       context->m_texture_system  = TextureSystem::create();
     }
     /* end of creating context dependencies */
