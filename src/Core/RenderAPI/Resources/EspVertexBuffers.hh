105230c430ca03d780d6ebecefc961e9b02a4dc5
sebastianSzarafin <sebastian.szarafin@interia.pl>
2023-11-28 19:55:52 +0100

    instancing: update render api to use instancing from within model component

--- src/Core/RenderAPI/Resources/EspVertexBuffers.hh
+++ src/Core/RenderAPI/Resources/EspVertexBuffers.hh
@@ -5,7 +5,7 @@
 
 namespace esp
 {
-  class EspVertexBuffers : EspBuffer
+  class EspVertexBuffers : public EspBuffer
   {
    public:
     static std::unique_ptr<EspVertexBuffers> create();
@@ -16,8 +16,15 @@ namespace esp
     EspVertexBuffers()          = default;
     virtual ~EspVertexBuffers() = default;
 
-    virtual void add(void* data, uint32_t vertex_size, uint32_t vertex_count) = 0;
-    virtual void attach()                                                     = 0;
+    virtual void add(void* data, uint32_t vertex_size, uint32_t vertex_count, BufferType type = LOCAL) = 0;
+    virtual void update(uint32_t index,
+                        void* data,
+                        uint32_t instance_size,
+                        uint32_t instance_count,
+                        uint64_t offset)                                                               = 0;
+    virtual void attach()                                                                              = 0;
+
+    virtual uint32_t get_buffers_count() = 0;
 
    private:
   };
--- src/Platform/Vulkan/Resources/VulkanVertexBuffers.cc
+++ src/Platform/Vulkan/Resources/VulkanVertexBuffers.cc
@@ -8,18 +8,45 @@ namespace esp
     return std::unique_ptr<VulkanVertexBuffers>(new VulkanVertexBuffers());
   }
 
-  void VulkanVertexBuffers::add(void* data, uint32_t vertex_size, uint32_t vertex_count)
+  void VulkanVertexBuffers::add(void* data, uint32_t vertex_size, uint32_t vertex_count, BufferType type)
   {
+    VkMemoryPropertyFlags memory_property_flags;
+    switch (type)
+    {
+    case LOCAL:
+      memory_property_flags = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+      break;
+    case VISIBLE:
+      memory_property_flags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
+      break;
+    }
+
     auto vulkan_buffer =
         VulkanBuffer::create_and_fill(data,
                                       vertex_size,
                                       vertex_count,
                                       VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
-                                      VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+                                      memory_property_flags);
 
     m_buffers.push_back(vulkan_buffer->get_buffer());
     m_offsets.push_back(0);
     m_vertex_buffers.push_back(std::move(vulkan_buffer));
+  }
+
+  void VulkanVertexBuffers::update(uint32_t index,
+                                   void* data,
+                                   uint32_t instance_size,
+                                   uint32_t instance_count,
+                                   uint64_t offset)
+  {
+    auto size = instance_size * instance_count;
+    ESP_ASSERT(index < m_vertex_buffers.size(), "Index was out of range")
+    ESP_ASSERT(m_vertex_buffers[index]->get_buffer_size() >= size + offset,
+               "Data size + offset can't be larger than size of updated buffer")
+
+    m_vertex_buffers[index]->map(size, offset); // TODO: it might be better to map this buffer permanently in 'add'
+    m_vertex_buffers[index]->write_to_buffer(data, size, offset);
+    m_vertex_buffers[index]->unmap();
   }
 
   void VulkanVertexBuffers::attach()
