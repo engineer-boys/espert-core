aece0daf22c63eb24460aec308bac3234af85660
obroslakjakub <obroslakjakub@gmail.com>
2023-12-23 14:58:08 +0100

    pipeline: add specialization constants to vulkan pipelines

--- src/Core/Utils/Utils.hh
+++ src/Core/Utils/Utils.hh
@@ -44,6 +44,11 @@ namespace esp
     std::hash<T> hasher;
     seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
   }
+
+  template<class... Ts> struct overloaded : Ts...
+  {
+    using Ts::operator()...;
+  };
 } // namespace esp
 #include <bitset>
 
