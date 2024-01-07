f38c4e3a8d7075d17980018d9f332e420206182d
kubo11 <jakub.obroslak@onet.pl>
2024-01-07 14:09:40 +0100

    changes to keep compatibility with linux

--- tests/app.cc
+++ tests/app.cc
@@ -13,16 +13,15 @@ TEST_CASE("App - run", "[app]")
     test_app->set_context(std::move(context));
     std::thread app_thread(&TestApp::run, test_app);
 
+#ifdef _WIN32
     // ignore windows warning: invalid validation layer name for layers:
     // GalaxyOverlayVkLayer
     // GalaxyOverlayVkLayer_VERBOSE
     // GalaxyOverlayVkLayer_DEBUG
-    if (WIN32) {
-      REQUIRE(esp::EspDebugMessenger::get_warning_count() <= 3);
-    }
-    else {
-      REQUIRE(esp::EspDebugMessenger::get_warning_count() == 0);
-    }
+    REQUIRE(esp::EspDebugMessenger::get_warning_count() <= 3);
+#else
+    REQUIRE(esp::EspDebugMessenger::get_warning_count() == 0);
+#endif
     REQUIRE(esp::EspDebugMessenger::get_error_count() == 0);
 
     test_app->terminate();
